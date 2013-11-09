8b99b747db47b32912255cb3b47f9733d10ae8b2
Sam Echikson <samuel.echikson@gmail.com>
2013-11-09 12:06:30 -0500

    Initial Commit

---
+++ P3/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  P3
+//
+//  Created by Samuel Echikson on 11/9/13.
+//  Copyright (c) 2013 Samuel Echikson. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
